f284b7e08b22049d64a574b9cb7ba90ffcf531c9
Showkat Ali <quarkgluons@gmail.com>
2021-10-06 15:52:34 +0530

    Reached About Strings

--- Koans/AboutStrings.cs
+++ Koans/AboutStrings.cs
@@ -17,21 +17,21 @@ namespace DotNetCoreKoans.Koans
         public void DoubleQuotedStringsAreStrings()
         {
             var str = "Hello, World";
-            Assert.Equal(typeof(FillMeIn), str.GetType());
+            Assert.Equal(typeof(string), str.GetType());
         }
 
         [Step(2)]
         public void SingleQuotedStringsAreNotStrings()
         {
             var str = 'H';
-            Assert.Equal(typeof(FillMeIn), str.GetType());
+            Assert.Equal(typeof(char), str.GetType());
         }
 
         [Step(3)]
         public void CreateAStringWhichContainsDoubleQuotes()
         {
             var str = "Hello, \"World\"";
-            Assert.Equal(FILL_ME_IN, str.Length);
+            Assert.Equal(14, str.Length);
         }
 
         [Step(4)]
@@ -40,7 +40,7 @@ namespace DotNetCoreKoans.Koans
             //The @ symbol creates a 'verbatim string literal'. 
             //Here's one thing you can do with it:
             var str = @"Hello, ""World""";
-            Assert.Equal(FILL_ME_IN, str.Length);
+            Assert.Equal(14, str.Length);
         }
 
         [Step(5)]
@@ -48,7 +48,7 @@ namespace DotNetCoreKoans.Koans
         {
             var strA = @"Verbatim Strings can handle both ' and "" characters (when escaped)";
             var strB = "Verbatim Strings can handle both ' and \" characters (when escaped)";
-            Assert.Equal(FILL_ME_IN, strA.Equals(strB));
+            Assert.Equal(true, strA.Equals(strB));
         }
 
         [Step(6)]
@@ -64,8 +64,8 @@ broken line";
 
             // Make sure to use a literal string.
             // Escaped characters in verbatim strings are covered later.
-            var literalString = FILL_ME_IN;
-            Assert.Equal(FILL_ME_IN, verbatimString.Length);
+            var literalString = "I\r\nam a\r\nbroken line";
+            Assert.Equal(literalString.Length, verbatimString.Length);
 
             // For verbatim strings, the newline character used will depend on
             // whether the source file uses a \r\n or a \n ending and they have
